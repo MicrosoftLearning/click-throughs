d9514e979a34c3859ca78b3973b5e4657a7d474b
James Zhu <v-jameszhu@microsoft.com>
2024-06-20 10:27:34 +0800

    add folders for cecilia

---
+++ docs/ilt/create_and_run_a_custom_promptbook_20240508/story_content/68luV5zYuwi_captions.js
@@ -0,0 +1,1 @@
+window.globalProvideData('caption', '{"data":"WEBVTT%0D%0AKind:%20captions%0D%0ASource:%20Articulate%20Closed%20Captions%20Editor%0D%0ASource%20Version:%203.87.32265.0%0D%0A%0D%0A00:00:00.150%20--%3E%2000:00:04.624%0D%0AThank%20you%20for%20completing%20this%20interactive%20guide.%20%20Select%20exit%20to%20close%20the%20page.%0D%0A%0D%0A"}')
---
+++ docs/ilt/create_and_run_a_custom_promptbook_20240508/story_content/6CyWhsEFnxY_captions.js
@@ -0,0 +1,1 @@
+window.globalProvideData('caption', '{"data":"WEBVTT%0D%0AKind:%20captions%0D%0ASource:%20Articulate%20Closed%20Captions%20Editor%0D%0ASource%20Version:%203.86.32080.0%0D%0A%0D%0A00:00:00.150%20--%3E%2000:00:02.743%0D%0ATo%20create%20the%20custom%20promptbook,%20select%20create.%0D%0A%0D%0A"}')

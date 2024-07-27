72e16a044094ba5254c60bc8dadc7b8e96785f66
James Zhu <v-jameszhu@microsoft.com>
2024-07-27 18:37:35 +0800

    update Sc-5006

---
+++ docs/Sc-5006/create_and_run_a_custom_promptbook/story_content/6ZO7TJmpaOp_captions.js
@@ -0,0 +1,1 @@
+window.globalProvideData('caption', '{"data":"WEBVTT%0D%0AKind:%20captions%0D%0ASource:%20Articulate%20Closed%20Captions%20Editor%0D%0ASource%20Version:%203.87.32265.0%0D%0A%0D%0A00:00:00.150%20--%3E%2000:00:04.545%0D%0AA%20drop%20down%20menu%20shows%20the%20actions%20you%20can%20take.%20%20%20%0D%0ASelect%20details.%0D%0A%0D%0A"}')
---
+++ docs/Sc-5006/create_and_run_a_custom_promptbook/story_content/6n1bBi0EUeu_captions.js
@@ -0,0 +1,1 @@
+window.globalProvideData('caption', '{"data":"WEBVTT%0D%0AKind:%20captions%0D%0ASource:%20Articulate%20Closed%20Captions%20Editor%0D%0ASource%20Version:%203.87.32265.0%0D%0A%0D%0A00:00:00.150%20--%3E%2000:00:01.933%0D%0AFor%20now%20leave%20the%20setting%20to%20Just%20me.%0D%0A%0D%0A"}')

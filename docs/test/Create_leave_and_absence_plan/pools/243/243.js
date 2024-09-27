b40a3ade38a861f2fc72a8bab297666c7a5b4da0
qijiexue <44390740+QijieXue@users.noreply.github.com>
2024-09-27 09:51:18 +0800

    Uploaded 3 simulations for Oscar

---
+++ docs/test/Create_leave_and_absence_plan/pools/243/243.js
@@ -0,0 +1,15 @@
+window.CPPool243Init = function() { cp.model['243Data']={
+};cp.model.data['243']={
+pqs:[],
+gqs:[],
+sqs:[],
+rgqs:[],
+rsqs:[],
+hasCC:false
+};
+cp.poolResources["243Images"]=[];
+cp.poolResources["243Videos"]=[
+];
+cp.poolResources["243SlideVideos"]=[
+];
+}

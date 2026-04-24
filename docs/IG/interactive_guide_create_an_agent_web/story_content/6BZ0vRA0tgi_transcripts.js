86638fca7780cb139c63802bd7ab4971e6febb8c
v-baosu <v-baosu@microsoft.com>
2026-04-24 09:29:04 +0800

    add interactive_guide_create_an_agent_web

---
+++ docs/IG/interactive_guide_create_an_agent_web/story_content/6BZ0vRA0tgi_transcripts.js
@@ -0,0 +1,5 @@
+
+(function() {
+    const data = {"transcripts":[{"name":"captions","cues":[{"start":960,"text":"Datum Corporation has deployed Microsoft Security Copilot "},{"start":4792,"text":"to enhance its security operations and plans to use Copilot agents."},{"start":11208,"text":"Copilot agents integrate with Microsoft Security solutions "},{"start":14801,"text":"and the partner ecosystem to automate security tasks."},{"start":18944,"text":"These agents are available in both \r\nstandalone "},{"start":21792,"text":"and embedded experiences and can run "},{"start":24001,"text":"automatically or on demand."}]}]};
+    window.globalLoadJsAsset('story_content/6BZ0vRA0tgi_transcripts.js', JSON.stringify(data));
+})();

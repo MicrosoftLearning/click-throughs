56a14724712af447c6dfff2fbbec5c56bb7e41fb
pablorodMS <v-pablorod@microsoft.com>
2026-04-30 15:09:19 -0600

    adding 2 IGs cecilia
    WITs 164064 and 164065

---
+++ docs/IG/interactive-guide-analyze-artifacts-investigations-in-defender-web/story_content/6BhBNYR55N6_transcripts.js
@@ -0,0 +1,5 @@
+
+(function() {
+    const data = {"transcripts":[{"name":"captions","cues":[{"start":4119,"text":"Welcome."},{"start":4976,"text":"In this interactive guide,"},{"start":6565,"text":"you’ll analyze alerts in Microsoft Defender XDR "},{"start":10016,"text":"to identify key artifacts and investigate suspicious activity"},{"start":13929,"text":"using Security Copilot."},{"start":15980,"text":"This interactive guide takes approximately 10 minutes to complete."},{"start":19624,"text":"Select Begin to continue."}]}]};
+    window.globalLoadJsAsset('story_content/6BhBNYR55N6_transcripts.js', JSON.stringify(data));
+})();
---
+++ docs/IG/interactive-guide-analyze-artifacts-investigations-in-defender-web/story_content/6cegwkdIAzx_transcripts.js
@@ -0,0 +1,5 @@
+
+(function() {
+    const data = {"transcripts":[{"name":"captions","cues":[{"start":1320,"text":"In this interactive guide,"},{"start":2944,"text":"you’ll analyze alerts, related artifacts, "},{"start":5764,"text":"and investigations in Microsoft Defender XDR. "},{"start":9576,"text":"You’ll review alert details, examine device and user context, "},{"start":13834,"text":"and use Security Copilot to support advanced investigation."},{"start":18092,"text":"When you’re ready, select Start to begin."}]}]};
+    window.globalLoadJsAsset('story_content/6cegwkdIAzx_transcripts.js', JSON.stringify(data));
+})();
